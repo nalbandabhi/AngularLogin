76553c93e2da7cbbe07ff492ad8d110c2f0a5508
nalbandabhi <nalbandabhinaykumar@gmail.com>
2023-04-09 18:18:04 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CustRegistrationComponent } from './components/cust-registration/cust-registration.component';
+import { CustLoginComponent } from './components/cust-login/cust-login.component';
+import { BillerLoginComponent } from './components/biller-login/biller-login.component';
+import { BillRegistrationComponent } from './components/bill-registration/bill-registration.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ForgotpassComponent } from './components/forgotpass/forgotpass.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustRegistrationComponent,
+    CustLoginComponent,
+    BillerLoginComponent,
+    BillRegistrationComponent,
+    HeaderComponent,
+    FooterComponent,
+    ForgotpassComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
